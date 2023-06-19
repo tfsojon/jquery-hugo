edf359858d19d542872259aed29379ca10244ed6
Sojon <sojon.themefisher@gmail.com>
2023-06-19 14:25:10 +0600

    init jquery+hugo

---
+++ themes/jquery-hugo/assets/js/script.js
@@ -0,0 +1,21 @@
+// Passive event listeners
+jQuery.event.special.touchstart = {
+  setup: function (_, ns, handle) {
+    this.addEventListener("touchstart", handle, {
+      passive: !ns.includes("noPreventDefault"),
+    });
+  },
+};
+jQuery.event.special.touchmove = {
+  setup: function (_, ns, handle) {
+    this.addEventListener("touchmove", handle, {
+      passive: !ns.includes("noPreventDefault"),
+    });
+  },
+};
+
+(function ($) {
+  "use strict";
+
+  console.log("Script Loaded!")
+})(jQuery);
